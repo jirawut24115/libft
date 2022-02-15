adbbcb1d9a48dd04b1f1c45c06e58ba0925109fc
Jack <jirawut24115@gmail.com>
2022-02-16 00:53:17 +0700

    add ft_isalpha ft_isdigit ft_isalnum ft_isascii ft_isprint ft_strlen header.h

---
+++ header.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalpha(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+int ft_strlen(char *s);
+#endif
